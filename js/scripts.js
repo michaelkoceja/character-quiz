b6764d24e8ae0f42bb317d743ec941dfee6cff7d
Michael Koceja <michael.koceja@gmail.com>
2017-04-27 14:27:36 -0700

    update html, working on js/scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(function() {
+  $("#options").submit(function(event) {
+    event.preventDefault();
 
+    var lightsaber = $("input:radio[name=lightsaber]:checked").val();
+    var blue = $("input:radio[name=blue]:checked").val();
+
+    if ("lightsaber" && "blue") {
+      $("#obi").show();
+    }
+    // console.log(weaponInput);
+  });
 });
